fb86d03a188340c6a9ecdb5691e9bb6be797612d
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2021-11-22 09:35:45 +0200

    chore: copy [ ./js/**.js ] files into [ ./docs ] folder

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("images/**/*.(png|jgp|jpeg)");
-  
+
+    eleventyConfig.addPassthroughCopy("js/**.js");
+
     return { dir: { layouts: "../layouts" } }
   
   };
