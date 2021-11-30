fa66bb2e6a63f2aec355770200eaca4645e36262
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2021-11-30 18:40:55 +0200

    chore( libraries ): download ( bootstrap 5 )
        - add [ ./css ]
        - copy [ ./css ] into [ ./docs ]

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images/**/*.(png|jgp|jpeg)");
 
     eleventyConfig.addPassthroughCopy("js/**.js");
+    eleventyConfig.addPassthroughCopy("css/**/*.css");
 
     return { dir: { layouts: "../layouts" } }
   
